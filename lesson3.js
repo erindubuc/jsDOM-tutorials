b5d5f96dc4e472faf9a61e9693658171852d1f2c
Erin Dubuc <erin.dubuc9@gmail.com>
2018-08-09 17:15:50 +0000

    lesson 3 getting elements by class/tag names

---
+++ lesson3.js
@@ -0,0 +1,24 @@
+//Getting element by id// 
+document.getElementById('page-banner');
+var banner = document.getElementById('page-banner');
+
+//Getting element by id//
+document.getElementById('book-list');
+var bookList = document.getElementById('book-list');
+
+// Getting multiple elements by class//
+document.getElementsByClassName('title');
+var titles = document.getElementsByClassName('title');
+
+// Getting multiple elements by tag name//
+var lis = document.getElementsbyTagName('li');
+
+
+var titles = document.getElementsByClassName('title');
+
+// console.log(Array.isArray(titles));
+// console.log(Array.isArray(Array.from(titles)));
+
+Array.from(titles).forEach(function(item){
+    console.log(item);
+})
