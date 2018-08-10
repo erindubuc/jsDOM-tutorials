0c765f6de07dec54837390b3638406b92b15cec0
Erin Dubuc <erin.dubuc9@gmail.com>
2018-08-10 01:51:44 +0000

    lesson4 create array & forEach

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-//Getting element by id// 
-document.getElementById('page-banner');
+// query can grab an element 
+const wmf = document.querySelector('#book-list li:nth-child(2).name');
+// console.log(wmf);
 
-var banner = document.getElementById('page-banner');
+// make variable to store books in, but this will only grab one
+var books = document.querySelector('#book-list li .name');
+// console.log(books);
+ 
+// this selector can grab ALL of the elements in the list
+books = document.querySelectorAll('#book-list li .name');
+// console.log(books);
 
-//Getting element by id//
-document.getElementById('book-list');
-
-var bookList = document.getElementById('book-list');
+// now, this function can display the array
+Array.from(books).forEach(function(book) {
+    console.log(book);
+});
